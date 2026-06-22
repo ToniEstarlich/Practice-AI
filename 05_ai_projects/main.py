5509d71cd1502a9ebda503b9746fd92e1d165a33
ToniEstarlich <110102891+ToniEstarlich@users.noreply.github.com>
2026-06-22 14:41:42 +0100

    camara AI in 05_ai_projects

---
+++ 05_ai_projects/main.py
@@ -0,0 +1,21 @@
+from ultralytics import YOLO
+import cv2
+
+model = YOLO("yolov8n.pt")
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    success, frame = camera.read()
+
+    results = model(frame)
+
+    annotated = results[0].plot()
+
+    cv2.imshow("AI Camera", annotated)
+
+    if cv2.waitKey(1) == 27:
+        break
+
+camera.release()
+cv2.destroyAllWindows()
